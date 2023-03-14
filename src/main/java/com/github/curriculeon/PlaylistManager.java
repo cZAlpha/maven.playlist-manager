e3f8c1d28498b72fc203ef44f47fe293528a3a9e
Noah <nbklaus21@students.desu.edu>
2023-03-14 18:36:55 -0400

    Got one more test working somehow

--- src/main/java/com/github/curriculeon/PlaylistManager.java
+++ src/main/java/com/github/curriculeon/PlaylistManager.java
@@ -1,13 +1,13 @@
 package com.github.curriculeon;
 
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class PlaylistManager {
     // Init. a list to hold the playlist stuff
     private List<String> playlist;
-
 
     public PlaylistManager(String[] songs) {
         this.playlist = Arrays.asList(songs); // Sets the list that holds the playlist equal to a list version of the array input
@@ -18,7 +18,7 @@ public class PlaylistManager {
     }
 
     public PlaylistManager() {
-        // Empty nullary constructor
+        this.playlist = null; // Nullary constructor doing nullary shit
     }
 
     public Playlist getPlaylist() {
@@ -35,17 +35,26 @@ public class PlaylistManager {
     }
 
     private void setPlaylist(String[] toArray) {
-        //if ( playlist != null ){
-            //this.playlist = Arrays.asList(playlist);
-        //}
+        if (playlist != null) {
+            this.playlist = Arrays.asList(toArray);
+        } else {
+            System.out.println("The playlist inputted is equal to null, please try again.");
+            throw new java.lang.IllegalArgumentException();
+        }
     }
 
     public void addSong(String songToAdd) {
-        playlist.add(songToAdd);
+        if ( this.playlist != null) {
+            List<String> arrayList = new ArrayList<>(this.playlist);
+            arrayList.add(songToAdd);
+            this.playlist = arrayList;
+        }
     }
 
     public void removeSong(String songToRemove) {
-        playlist.remove(songToRemove);
+        if ( this.playlist != null) {
+            playlist.remove(songToRemove);
+        }
     }
 
     public void printAll() {
